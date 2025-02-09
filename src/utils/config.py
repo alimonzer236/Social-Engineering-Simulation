cfcf8c09a04a629528ee86f79583f9c3e5a9ad90
ali <123268150+alimonzer236@users.noreply.github.com>
2025-02-09 14:28:07 +0200

    Create config.py

---
+++ src/utils/config.py
@@ -0,0 +1,6 @@
+# Configuration settings for the simulation
+SIMULATION_SETTINGS = {
+    "phishing_email": "http://fake-link.com",
+    "pretexting_message": "Hello, this is IT support. We need your password to fix an issue.",
+    "baiting_offer": "Free USB drive! Plug it into your computer to claim your gift."
+}
